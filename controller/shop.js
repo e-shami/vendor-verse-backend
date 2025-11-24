3b485f5ceda1a7207b09d0ff90bb4a46cbd55155
Ehtisham Rehmat <a7rehmat@gmail.com>
2025-11-24 22:59:24 +0500

    finnally fixed the cors bug and now the back end works both in local and in production mode

--- controller/shop.js
+++ controller/shop.js
@@ -39,7 +39,7 @@ router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+    const activationUrl = `${(process.env.NODE_ENV !== "PRODUCTION") ? "http://localhost:3000" : "https://vendor-verse-phi.vercel.app"}/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
